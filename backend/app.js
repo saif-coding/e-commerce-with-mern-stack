3b146d0fcbd518faaaa85ab6d04e1dd9512c50a7
saif ullah <saifullaha730@gmail.com>
2026-02-08 12:17:50 +0500

    same change

--- backend/app.js
+++ backend/app.js
@@ -15,9 +15,9 @@ connectDB();
 app.use(express.json());
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: "https://e-commerce-with-mern-stack.onrender.com",
     credentials: true,
-  })
+  }),
 );
 app.use(cookieParser());
 
